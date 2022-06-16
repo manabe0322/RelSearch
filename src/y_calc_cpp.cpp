8794856e786322b43695290f7ff826dbc31febc6
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-06-16 15:09:22 +0900

    Add sumL

--- src/y_calc_cpp.cpp
+++ src/y_calc_cpp.cpp
@@ -6,13 +6,16 @@ using namespace Rcpp;
 IntegerMatrix matchY2(CharacterVector qHap, CharacterVector rHap) {
   int nL = qHap.length();
   IntegerMatrix judgeMat(3, nL + 1);
+  int sumL = 0;
   for(int i = 0; i < nL; ++i){
     String qAl(qHap[i]);
     String rAl(rHap[i]);
     bool matchQR = qAl == rAl;
     if(matchQR == false){
       judgeMat(0, i) = 1;
+      sumL = sumL + 1;
     }
   }
+  judgeMat(0, nL) = sumL;
   return(judgeMat);
 }
