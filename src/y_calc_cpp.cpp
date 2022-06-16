d2e0aee8f960b685361987387034410259519eb8
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-06-16 14:16:04 +0900

    String

--- src/y_calc_cpp.cpp
+++ src/y_calc_cpp.cpp
@@ -1,15 +1,13 @@
 #include <Rcpp.h>
-#include <string>
 using namespace Rcpp;
-using namespace std;
 
 // [[Rcpp::export]]
 IntegerMatrix matchY2(CharacterVector qHap, CharacterVector rHap) {
   int nL = qHap.length();
   IntegerMatrix judgeMat(3, nL + 1);
   for(int i = 0; i < nL; ++i){
-    string qAl = qHap[i];
-    string rAl = rHap[i];
+    String qAl(qHap[i]);
+    String rAl(rHap[i]);
     bool matchQR = qAl == rAl;
     if(matchQR == true){
       judgeMat(0, i) = 1;
