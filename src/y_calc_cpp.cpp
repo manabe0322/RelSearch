408acb273a5212d0f9a8813f3ffc9aee7eb0d728
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-06-16 14:00:01 +0900

    matchY2

--- src/y_calc_cpp.cpp
+++ src/y_calc_cpp.cpp
@@ -3,5 +3,15 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 IntegerMatrix matchY2(CharacterVector qHap, CharacterVector rHap) {
-  int nH = qHap.length();
+  int nL = qHap.length();
+  IntegerMatrix judgeMat(3, nL + 1);
+  for(int i = 0; i < nL; ++i){
+    string qAl = qHap[i];
+    string rAl = rHap[i];
+    bool matchQR = qAl == rAl;
+    if(matchQR == true){
+      judgeMat(0, i) = 1
+    }
+  }
+  return(judgeMat);
 }
