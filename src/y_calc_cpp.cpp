26949f1d868cb69d29469feaac8939fa5f69d12a
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-06-16 14:04:27 +0900

    matchY2

--- src/y_calc_cpp.cpp
+++ src/y_calc_cpp.cpp
@@ -6,11 +6,11 @@ IntegerMatrix matchY2(CharacterVector qHap, CharacterVector rHap) {
   int nL = qHap.length();
   IntegerMatrix judgeMat(3, nL + 1);
   for(int i = 0; i < nL; ++i){
-    string qAl = qHap[i];
-    string rAl = rHap[i];
+    char qAl = qHap[i];
+    char rAl = rHap[i];
     bool matchQR = qAl == rAl;
     if(matchQR == true){
-      judgeMat(0, i) = 1
+      judgeMat(0, i) = int 1
     }
   }
   return(judgeMat);
