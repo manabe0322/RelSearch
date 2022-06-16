242e6f21e71eedfb0e66e615ddbda52c9a2f977f
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-06-16 14:34:37 +0900

    importFrom

--- R/package.R
+++ R/package.R
@@ -1,6 +1,7 @@
 #' relsearch: Open-source software for screening relatives between query and reference databases
 #'
 #' @importFrom readr parse_number
+#' @importFrom tcltk2 tk2notebook tk2notetab.select tk2mclistbox tk2column tk2insert.multi
 "_PACKAGE"
 
 #' @useDynLib relsearch, .registration = TRUE
