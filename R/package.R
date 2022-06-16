11cbb195ff60f07992f7f6bd2cae69f9294d9ba1
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-06-16 13:35:48 +0900

    Set Rcpp

--- R/package.R
+++ R/package.R
@@ -1,4 +1,7 @@
-#' KinSN: Open-source software for screening relatives between query and reference databases
+#' relsearch: Open-source software for screening relatives between query and reference databases
 #'
 #' @importFrom readr parse_number
 "_PACKAGE"
+
+#' @useDynLib relsearch, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
