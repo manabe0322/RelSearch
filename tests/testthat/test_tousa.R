7e726bc6382cc887f28e7563213877a390eba88d
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-06-21 11:22:30 +0900

    Test of tousa

---
+++ tests/testthat/test_tousa.R
@@ -0,0 +1,18 @@
+test_that("tousa pattern 1", {
+  vec <- tousa(3, 12, 3)
+  expect_equal(length(vec), 4)
+  expect_equal(vec[1], 3)
+  expect_equal(vec[2], 6)
+  expect_equal(vec[3], 9)
+  expect_equal(vec[4], 12)
+})
+
+test_that("tousa pattern 2", {
+  vec <- tousa(3, 11, 2)
+  expect_equal(length(vec), 5)
+  expect_equal(vec[1], 3)
+  expect_equal(vec[2], 5)
+  expect_equal(vec[3], 7)
+  expect_equal(vec[4], 9)
+  expect_equal(vec[5], 11)
+})
