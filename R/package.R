1f59177ad4d0922a28bbd8d0b1f4f3b637d36777
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2023-06-14 18:41:57 +0900

    Add a package "data.table"

--- R/package.R
+++ R/package.R
@@ -1,5 +1,6 @@
 #' relsearch: Open-source software for screening relatives between query and reference databases
 #'
+#' @importFrom data.table data.table fread
 #' @importFrom readr parse_number
 "_PACKAGE"
 
