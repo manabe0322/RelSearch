bacb98466673b447515eccf9dedf39c07cf1b2a5
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2023-06-29 13:17:19 +0900

    Update testthat for make_dummy_gt

--- tests/testthat/test_make_dummy_gt.R
+++ tests/testthat/test_make_dummy_gt.R
@@ -80,3 +80,19 @@ test_that("makeDummyGt 6", {
   expect_equal(dummy_gt[[3]][2], 99)
 })
 
+test_that("makeDummyGt 7", {
+  vgt <- 12
+  rgt <- c(10, 11)
+  uniq_vr_al <- sort(unique(c(vgt, rgt)))
+  dummy_gt <- make_dummy_gt(vgt, uniq_vr_al)
+  expect_equal(length(dummy_gt), 4)
+  expect_equal(dummy_gt[[1]][1], 10)
+  expect_equal(dummy_gt[[1]][2], 12)
+  expect_equal(dummy_gt[[2]][1], 11)
+  expect_equal(dummy_gt[[2]][2], 12)
+  expect_equal(dummy_gt[[3]][1], 12)
+  expect_equal(dummy_gt[[3]][2], 12)
+  expect_equal(dummy_gt[[4]][1], 12)
+  expect_equal(dummy_gt[[4]][2], 99)
+})
+
