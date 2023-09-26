bca60e0cc459c793d1a43cf9f6543790e3dfb9f3
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2023-09-26 10:45:45 +0900

    Add functions to download example files

--- R/launch_app.R
+++ R/launch_app.R
@@ -288,7 +288,7 @@ relsearch <- function(){
                                         tabPanel("mtDNA : Reference", dataTableOutput("view_dt_r_mt")),
                              ),
 
-                             navbarMenu("Tools",
+                             navbarMenu("Settings",
                                         tabPanel("Criteria",
                                                  titlePanel("Criteria"),
                                                  br(),
@@ -319,9 +319,7 @@ relsearch <- function(){
                                         tab_myu_ui("tab_myu"),
 
                                         tabPanel("Analysis method for STR",
-
                                                  titlePanel("Analysis method for STR"),
-
                                                  numeric_ui("maf"),
                                                  numeric_ui("pd_v"),
                                                  numeric_ui("pd_r"),
@@ -329,6 +327,29 @@ relsearch <- function(){
                                                  h4("Update default"),
                                                  actionButton("act_par_auto_update", label = "Update default")
                                         )
+                             ),
+
+                             tabPanel("Example files",
+                                      titlePanel("Example files"),
+                                      downloadButton("download_v_auto", "STR victim database"),
+                                      br(),
+                                      br(),
+                                      downloadButton("download_r_auto", "STR reference database"),
+                                      br(),
+                                      br(),
+                                      downloadButton("download_af", "Allele frequencies"),
+                                      br(),
+                                      br(),
+                                      downloadButton("download_v_y", "Y-STR victim database"),
+                                      br(),
+                                      br(),
+                                      downloadButton("download_r_y", "Y-STR reference database"),
+                                      br(),
+                                      br(),
+                                      downloadButton("download_v_mt", "mtDNA victim database"),
+                                      br(),
+                                      br(),
+                                      downloadButton("download_r_mt", "mtDNA reference database")
                              ),
 
                              tabPanel("Manual",
@@ -715,6 +736,81 @@ relsearch <- function(){
         )
       })
     })
+
+    #################
+    # Example files #
+    #################
+
+    output$download_v_auto <- downloadHandler(
+      filename = "str_victim_example.csv",
+      content = function(file){
+        csvfile <- read.csv(paste0(path_pack, "/extdata/examples/str_victim_example.csv"))
+        mk <- setdiff(colnames(csvfile), "SampleName")
+        id_mk <- 1:length(mk)
+        mk <- mk[id_mk[id_mk %% 2 == 1]]
+        colnames(csvfile) <- c("SampleName", as.vector(sapply(mk, rep, 2)))
+        csvfile[is.na(csvfile)] <- ""
+        write.csv(csvfile, file, row.names = FALSE)
+      }
+    )
+
+    output$download_r_auto <- downloadHandler(
+      filename = "str_ref_example.csv",
+      content = function(file){
+        csvfile <- read.csv(paste0(path_pack, "/extdata/examples/str_ref_example.csv"))
+        mk <- setdiff(colnames(csvfile), c("SampleName", "Relationship"))
+        id_mk <- 1:length(mk)
+        mk <- mk[id_mk[id_mk %% 2 == 1]]
+        colnames(csvfile) <- c("SampleName", "Relationship", as.vector(sapply(mk, rep, 2)))
+        csvfile[is.na(csvfile)] <- ""
+        write.csv(csvfile, file, row.names = FALSE)
+      }
+    )
+
+    output$download_af <- downloadHandler(
+      filename = "str_af_example.csv",
+      content = function(file){
+        csvfile <- read.csv(paste0(path_pack, "/extdata/examples/str_af_example.csv"))
+        csvfile[is.na(csvfile)] <- ""
+        write.csv(csvfile, file, row.names = FALSE)
+      }
+    )
+
+    output$download_v_y <- downloadHandler(
+      filename = "y_victim_example.csv",
+      content = function(file){
+        csvfile <- read.csv(paste0(path_pack, "/extdata/examples/y_victim_example.csv"))
+        csvfile[is.na(csvfile)] <- ""
+        write.csv(csvfile, file, row.names = FALSE)
+      }
+    )
+
+    output$download_r_y <- downloadHandler(
+      filename = "y_ref_example.csv",
+      content = function(file){
+        csvfile <- read.csv(paste0(path_pack, "/extdata/examples/y_ref_example.csv"))
+        csvfile[is.na(csvfile)] <- ""
+        write.csv(csvfile, file, row.names = FALSE)
+      }
+    )
+
+    output$download_v_mt <- downloadHandler(
+      filename = "mt_victim_example.csv",
+      content = function(file){
+        csvfile <- read.csv(paste0(path_pack, "/extdata/examples/mt_victim_example.csv"))
+        csvfile[is.na(csvfile)] <- ""
+        write.csv(csvfile, file, row.names = FALSE)
+      }
+    )
+
+    output$download_r_mt <- downloadHandler(
+      filename = "mt_ref_example.csv",
+      content = function(file){
+        csvfile <- read.csv(paste0(path_pack, "/extdata/examples/mt_ref_example.csv"))
+        csvfile[is.na(csvfile)] <- ""
+        write.csv(csvfile, file, row.names = FALSE)
+      }
+    )
 
     ####################
     # Perform analysis #
