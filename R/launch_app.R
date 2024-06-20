be87bcf50e8bf697dabeabc0096f73b72c28bc91
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2024-06-20 14:40:37 +0900

    test

--- R/launch_app.R
+++ R/launch_app.R
@@ -4,6 +4,7 @@
 #' @usage RelSearch()
 #' @export
 RelSearch <- function(){
+  # Test
   ver_soft <- packageVersion("RelSearch")
   path_pack <- path.package("RelSearch", quiet = FALSE)
   options(shiny.maxRequestSize = 500 * 1024^2)
