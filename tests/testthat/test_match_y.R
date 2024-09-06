9719940c300c1cc5dbd39f40abb5ee9e30bb3b6d
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2024-09-06 18:11:29 +0900

    v0.18.2 add test

--- tests/testthat/test_match_y.R
+++ tests/testthat/test_match_y.R
@@ -127,3 +127,29 @@ test_that("match_y pattern 5", {
   expect_equal(result[[3]][3], 2)
   expect_equal(result[[3]][4], 3)
 })
+
+test_that("match_y pattern 6", {
+
+  # Condition
+  v_hap <- c("10", "10, 13", "13, 14")
+  r_hap <- c("10, 11, 12", "10, 11, 12", "10, 11, 12")
+
+  # Run
+  result <- match_y(v_hap, r_hap)
+
+  # Test
+  expect_equal(length(result), 3)
+  expect_equal(length(result[[1]]), 4)
+  expect_equal(result[[1]][1], 0)
+  expect_equal(result[[1]][2], 1)
+  expect_equal(result[[1]][3], 1)
+  expect_equal(result[[1]][4], 2)
+  expect_equal(result[[2]][1], 1)
+  expect_equal(result[[2]][2], 0)
+  expect_equal(result[[2]][3], 0)
+  expect_equal(result[[2]][4], 1)
+  expect_equal(result[[3]][1], 0)
+  expect_equal(result[[3]][2], 1)
+  expect_equal(result[[3]][3], 99)
+  expect_equal(result[[3]][4], 100)
+})
