9f1248f6d44fc3bf106d77c89e5a3bcf7580c8d9
Sho Manabe <manabecchi0322@gmail.com>
2026-02-08 14:59:11 +0900

    v0.22.0 - Improve analyze_auto when show_progress = FALSE

--- R/analysis_auto.R
+++ R/analysis_auto.R
@@ -304,7 +304,10 @@ analyze_auto <- function(dt_v_auto, dt_r_auto, dt_af,
       value = 0
     )
   }else{
-    result_auto <- calc_kin_lr_all(gt_v_auto, gt_r_auto, assumed_rel_all, af_list, af_al_list, names_rel, pibds_rel, myus, cons_mutations, parent_victim)
+    result_auto <- calc_kin_lr_all(gt_v_auto, gt_r_auto, assumed_rel_all, af_list, af_al_list, names_rel, pibds_rel,
+                                   myus_paternal_m2, myus_paternal_m1, myus_paternal_0, myus_paternal_p1, myus_paternal_p2,
+                                   myus_maternal_m2, myus_maternal_m1, myus_maternal_0, myus_maternal_p1, myus_maternal_p2,
+                                   bool_pc_all, bool_parent_victim_all, bool_parent_male_all)
   }
 
   ##########################################################
